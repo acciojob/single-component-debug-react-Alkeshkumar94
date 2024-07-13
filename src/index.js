b54aacbe6d77706036e80ffc0c15bbae8ce83379
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-07-13 19:28:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 // src/index.js
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
